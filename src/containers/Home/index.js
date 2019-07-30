c8a25520326a572b3c908190d17fd3cf8ca9a7b6
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-30 14:30:46 -0400

    Change home text

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render() {
-    return <div>DEV react-frontend-starter!</div>
+    return <div>SCA react-frontend-starter!</div>
   }
 }
 
